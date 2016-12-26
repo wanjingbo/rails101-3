385cb2b068d433baa2fab4a23e8a5c6d744c0c0e
wanjingbo <wanbuhaogame@126.com>
2016-12-26 19:28:55 +0800

    join & quit group action

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -42,6 +42,33 @@ class GroupsController < ApplicationController
      redirect_to groups_path
    end
 
+   def join
+     @group = Group.find(params[:id])
+
+     if !current_user.is_member_of?(@group)
+       current_user.join!(@group)
+       flash[:notice] = "加入本讨论版成功"
+     else
+       flash[:warning] = "你已是本讨论组成员了！"
+     end
+
+     redirect_to group_path(@group)
+   end
+
+   def quit
+     @group = Group.find(params[:id])
+
+     if current_user.is_member_of?(@group)
+       current_user.quit!(@group)
+       flash[:alert] = "已推出讨论组"
+     else
+       flash[:warning] = "你不是本讨论组成员，怎么退出"
+     end
+
+     redirect_to group_path(@group)
+   end
+
+
    private
 
    def find_group_and_check_permission
